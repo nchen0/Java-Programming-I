bedb67eda8682163d5447d466452b3af72af7df9
nchen0 <nicky.chen0@gmail.com>
2019-06-29 09:36:54 -0400

    week 6

--- week6-104.Sorting/src/Main.java
+++ week6-104.Sorting/src/Main.java
@@ -2,6 +2,29 @@
 public class Main {
     public static void main(String[] args) {
         // write testcode here
+        // indexes:   0  1  2  3  4
+        int[] values = {6, 5, 8, 7, 11};
+        System.out.println("Index of the smallest: " + indexOfTheSmallest(values));
+    }
+
+    public static int smallest(int[] array) {
+       int infinity = Integer.MAX_VALUE;
+       for (int a : array) {
+           if (a < infinity) {
+               infinity = a;
+           }
+       }
+       return infinity;
+    }
+
+    public static int indexOfTheSmallest(int[] array) {
+        int smallestInt = smallest(array);
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == smallestInt) {
+                return i;
+            }
+        }
+        return -1;
     }
 
 }
